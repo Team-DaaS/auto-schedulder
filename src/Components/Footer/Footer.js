883a8d5501b217631e48ce2f4fb22973d6589088
Ryan Morgan <nerdmomsrock@gmail.com>
2021-11-02 21:55:27 -0400

    footer styling

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,21 +1,42 @@
 import React from "react";
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { ScheduleRounded } from "@material-ui/icons";
 
 export const Footer = () => {
   return (
-    <Box sx={{ backgroundColor: "#E8E8E8" }}>
+    <Box sx={{ backgroundColor: "#1976d2" }}>
       <Container maxWidth="lg">
         <Box p={{ xs: 4, md: 6, lg: 6 }}>
           <Grid container>
             <Grid item xs={12} md={3}>
-              Column 1
+              <Typography
+                component="h3"
+                variant="h8"
+                sx={{
+                  color: "white",
+
+                  marginLeft: -45,
+                }}
+              >
+                © 2021 Soccer Scheduler
+              </Typography>
             </Grid>
+            <Grid item xs={12} md={3}></Grid>
             <Grid item xs={12} md={3}>
-              Column 2
-            </Grid>
-            <Grid item xs={12} md={3}>
-              Column 3
+              <Typography
+                component="h5"
+                variant="h8"
+                sx={{
+                  color: "white",
+                  textAlign: "right",
+                  paddingRight: -20,
+                  marginRight: -75,
+                }}
+              >
+                Meeting your scheduling needs since the conclusion of
+                DevMountain WRPT2
+              </Typography>
             </Grid>
           </Grid>
         </Box>
