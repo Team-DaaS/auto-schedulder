a6f12cbff42ed25792587628e2819efaee04ba79
onava84 <oscar_nf@hotmail.com>
2021-11-04 21:04:35 -0500

    import-page

--- src/Components/Import/Import.js
+++ src/Components/Import/Import.js
@@ -1,8 +1,11 @@
 import React from "react";
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { useSelector } from "react-redux";
 import { Redirect } from "react-router";
+import { Button } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import UploadFileIcon from "@mui/icons-material/UploadFile";
 
 const Import = () => {
   const userId = useSelector((reduxState) => reduxState.userId);
@@ -12,7 +15,27 @@ const Import = () => {
   return (
     <Box>
       <Container>
-        <Box p={4}>Import</Box>
+        <Box pt={4} pb={4}>
+          <Typography variant="h4">Import players data</Typography>
+          <Typography>
+            Soccer goalie defender upper 90 hat trick referee forward pitch
+            halftime one-two African Cup of Nations midfielder chip goal number
+            10 red card. Defender brace referee soccer hat trick goal upper 90
+            center-half three-five-two number 10 African Cup of Nations pitch
+            halftime goalie forward red card.
+          </Typography>
+          <Box
+            sx={{ border: " solid 2px gray", borderRadius: "6px" }}
+            p={6}
+            mt={3}
+            mb={4}
+          >
+            <Button variant="outlined" startIcon={<UploadFileIcon />}>
+              Upload file...
+            </Button>
+          </Box>
+          <Button variant="contained">Save</Button>
+        </Box>
       </Container>
     </Box>
   );
